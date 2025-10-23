82d97f12231c34ac05d6ac0fa87fb6005940e98a
Anna-Xevanya <xevanya27@gmail.com>
2025-10-23 22:51:29 +0700

    Delete storage directory

--- storage/framework/views/c00c9c60c56c65bddaff257804674efe.php
+++
@@ -1,4 +0,0 @@
-<footer class="bg-dark text-white text-center py-3 mt-5">
-    <p class="mb-0">| Copyright &copy; <?php echo e(date('Y')); ?> K-Manhwa | </p>
-</footer>
-<?php /**PATH D:\laragon\www\tugas-laravel\resources\views/components/footer.blade.php ENDPATH**/ ?>
