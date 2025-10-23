c977784ed403a2eea22656700eba0bed00345b57
Anna-Xevanya <xevanya27@gmail.com>
2025-10-23 22:21:21 +0700

    Add files via upload

---
+++ storage/framework/views/c94cc18e4414aa7f4191e9277c2b6d49.php
@@ -0,0 +1,13 @@
+
+
+<?php $__env->startSection('title', 'Profile'); ?>
+
+<?php $__env->startSection('content'); ?>
+<div class="text-center">
+    <h3>Profil Pengguna</h3>
+    <p>Nama pengguna: <strong><?php echo e($username ?? 'Guest'); ?></strong></p>
+    <p>Email: <?php echo e(strtolower($username ?? 'guest')); ?>@example.com</p>
+</div>
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\laragon\www\tugas-laravel\resources\views/profile.blade.php ENDPATH**/ ?>
